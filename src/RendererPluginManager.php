f5a025400e453a1d5454cc2889f28bac23c15d15
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-11 15:20:46 -0600

    Updated code to work with zend-servicemanager 2.7
    This only needed to touch the plugin manager implementations. The following
    changes were made:
    
    - Introduced `$shareByDefault` property, with same value as `$sharedByDefault`.
    - Introduced implementations for both `validate()` and `validatePlugin()`.

--- src/RendererPluginManager.php
+++ src/RendererPluginManager.php
@@ -10,6 +10,7 @@
 namespace Zend\Barcode;
 
 use Zend\ServiceManager\AbstractPluginManager;
+use Zend\ServiceManager\Exception\InvalidServiceException;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 /**
@@ -22,7 +23,12 @@ use Zend\ServiceManager\Factory\InvokableFactory;
 class RendererPluginManager extends AbstractPluginManager
 {
     /**
-     * @var bool Ensure services are not shared
+     * @var bool Ensure services are not shared (v2)
+     */
+    protected $shareByDefault = false;
+
+    /**
+     * @var bool Ensure services are not shared (v3)
      */
     protected $sharedByDefault = false;
 
@@ -44,4 +50,37 @@ class RendererPluginManager extends AbstractPluginManager
     ];
 
     protected $instanceOf = Renderer\AbstractRenderer::class;
+
+    /**
+     * Validate the plugin is of the expected type (v3).
+     *
+     * Validates against `$instanceOf`.
+     *
+     * @param mixed $instance
+     * @throws InvalidServiceException
+     */
+    public function validate($instance)
+    {
+        if (! $instance instanceof $this->instanceOf) {
+            throw new InvalidServiceException(sprintf(
+                '%s can only create instances of %s; %s is invalid',
+                get_class($this),
+                $this->instanceOf,
+                (is_object($instance) ? get_class($instance) : gettype($instance))
+            ));
+        }
+    }
+
+    /**
+     * Validate the plugin is of the expected type (v2).
+     *
+     * Proxies to `validate()`.
+     *
+     * @param mixed $instance
+     * @throws InvalidServiceException
+     */
+    public function validatePlugin($instance)
+    {
+        $this->validate($instance);
+    }
 }
